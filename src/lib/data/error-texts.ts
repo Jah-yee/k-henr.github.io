134f753de2e383df7bd9657fe7336a4691f61a2b
k-henr <contact@khenr.se>
2026-09-08 19:53:07 +0200

    Error page

---
+++ src/lib/data/error-texts.ts
@@ -0,0 +1,5 @@
+export const errorTexts: { [key: number]: string } = {
+    200: "Okay wait, how did you even do this?? Contact me immediately so that I might learn your secrets",
+    404: "If you came here from a link on the website, please contact me and tell me which one so I can fix it!",
+    500: "This error signifies that I did something wrong on the server side, so please write to me and tell me what you did to get here so I can fix it!",
+};
---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { page } from "$app/state";
+    import { errorTexts } from "$lib/data/error-texts";
+</script>
+
+<section>
+    <h1>{page.status}: {page.error?.message}</h1>
+    {#if errorTexts[page.status] !== undefined}
+        <p>{errorTexts[page.status]}</p>
+    {/if}
+    <p><a href="/">Back to the home page</a></p>
+</section>
